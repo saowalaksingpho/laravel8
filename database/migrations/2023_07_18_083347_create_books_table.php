9abaf5e62834ff2c777854658e0aa24c8e905492
saowalaksingpho <125942204+saowalaksingpho@users.noreply.github.com>
2023-07-18 15:46:57 +0700

    week05

---
+++ database/migrations/2023_07_18_083347_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->integer('number')->nullable();
+            $table->float('total')->nullable();
+            $table->string('name')->nullable();
+            $table->date('date')->nullable();
+            $table->text('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}

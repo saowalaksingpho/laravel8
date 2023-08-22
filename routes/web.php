7dc17c8d7fb7e71e5ab3e45e546738701e0306ce
saowalaksingpho <125942204+saowalaksingpho@users.noreply.github.com>
2023-08-22 16:51:09 +0700

    week09.1

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\MyProfileController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController; 
+use App\Http\Controllers\UserController; 
+use App\Http\Controllers\VehicleController;  
 
 /*
 |--------------------------------------------------------------------------
@@ -120,3 +124,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+//Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
+// Route::resource('profile', 'ProfileController');
+// Route::resource('user', 'UserController');
+// Route::resource('vehicle', 'VehicleController');
+
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
